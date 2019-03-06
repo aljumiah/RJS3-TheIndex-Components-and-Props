fedd791fcd8eec464969c62e59f769e6003b18f1
Mohammed <m7amd.s3ad@hotmail.com>
2019-03-06 11:47:11 +0300

    last commit with good looping stratigy

--- src/AuthorCard.js
+++ src/AuthorCard.js
@@ -4,23 +4,29 @@ class AuthorCard extends Component {
   render() {
     return (
       <div className="col-lg-4 col-md-6 col-12">
-        {this.props.authors.map(elem => (
-          <div className="card">
-            <div className="image">
-              <img
-                className="card-img-top img-fluid"
-                src={elem.imageUrl}
-                alt={elem.first_name + " " + elem.last_name}
-              />
-            </div>
-            <div className="card-body">
-              <h5 className="card-title">
-                <span>{elem.first_name + " " + elem.last_name}</span>
-              </h5>
-              <small className="card-text">{elem.books.length} books</small>
-            </div>
+        <div className="card">
+          <div className="image">
+            <img
+              className="card-img-top img-fluid"
+              src={this.props.author.imageUrl}
+              alt={
+                this.props.author.first_name + " " + this.props.author.last_name
+              }
+            />
           </div>
-        ))}
+          <div className="card-body">
+            <h5 className="card-title">
+              <span>
+                {this.props.author.first_name +
+                  " " +
+                  this.props.author.last_name}
+              </span>
+            </h5>
+            <small className="card-text">
+              {this.props.author.books.length} books
+            </small>
+          </div>
+        </div>
       </div>
     );
   }
--- src/AuthorList.js
+++ src/AuthorList.js
@@ -2,16 +2,14 @@ import React, { Component } from "react";
 import AuthorCard from "./AuthorCard";
 class AuthorList extends Component {
   render() {
+    const cards = this.props.authors.map(author => (
+      <AuthorCard key={author.first_name} author={author} />
+    ));
     return (
       <div className="content col-10">
         <div className="authors">
           <h3>Authors</h3>
-          <div className="row">
-            <AuthorCard
-              key={this.props.authors.first_name}
-              authors={this.props.authors}
-            />
-          </div>
+          <div className="row">{cards}</div>
         </div>
       </div>
     );
